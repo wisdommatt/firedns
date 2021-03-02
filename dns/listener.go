046946a89117b0ad796a575ff3a2efbfbcf9c5b4
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-03-02 14:46:28 +0100

    Added a dummy ListenAndServe function to the dns package.

---
+++ dns/listener.go
@@ -0,0 +1,9 @@
+package dns
+
+import "net"
+
+// ListenAndServe listens for a request to retrieve a website's DNS
+// information and serves the information back to the client.
+func ListenAndServe(udpConn *net.UDPConn) {
+
+}
